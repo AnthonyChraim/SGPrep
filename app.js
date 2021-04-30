91db00eef9343b5c6152b14bc919e04e6e012de7
TonyMontana11 <anth.chraim@gmail.com>
2021-04-29 23:18:03 -0400

    Unknown type

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Anth';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {}
+}
+generateError('An error occurred!', 500);
